e34c91264b7a2b6ac6c4f92c2b704240def8c652
GABRIELDELASSIM <143609610+gabrieldelassim@users.noreply.github.com>
2025-04-08 20:39:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuirFonteBotao = document.getElementById('diminuir-fonte');
-  
-    let tamanhoAtualFonte = 1;
-  
-    aumentaFonteBotao.addEventListener('click', function () {
-      tamanhoAtualFonte += 0.1;
-      document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-    });
-  
-    diminuirFonteBotao.addEventListener('click', function () {
-      tamanhoAtualFonte -= 0.1;
-      document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-    });
-  });
-  
+const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+const alternaContraste = document.getElementById('alterna-contraste');
+const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
+alternaContraste.addEventListener('click', function () {
+  document.body.classList.toggle('alto-contraste');
+});
+
+botaoDeAcessibilidade.addEventListener('click', function () {
+  botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+  opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+
+  const aberto = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+  botaoDeAcessibilidade.setAttribute('aria-expanded', !aberto);
+});
